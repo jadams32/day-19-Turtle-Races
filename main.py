91822a583a92059ac21fbcde130390b36b7c0e0b
John Adams Jr <john.c.adams32@gmail.com>
2022-01-07 01:09:25 -0700

    winning logic added

--- main.py
+++ main.py
@@ -2,19 +2,35 @@
 # will reach the finish line first. Feel free to poke around a bit, and have fun betting!
 
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Place your bet!", prompt="Pick a turtle to win the race! Select color:")
-
+placed_bet = False
 colors = ["red", "orange", "green", "yellow", "blue", "purple"]
 y_locations = [160, 100, 40, -20, -80, -140]
-
+turtles = []
 for num in range(6):
-    john = Turtle(shape="turtle")
-    john.penup()
-    john.color(colors[num])
-    john.goto(x=-230, y=y_locations[num])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[num])
+    new_turtle.goto(x=-230, y=y_locations[num])
+    turtles.append(new_turtle)
+
+if user_bet:
+    placed_bet = True
+
+while placed_bet:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            placed_bet = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"The {winner} turtle has won! You win!")
+            else:
+                print(f"The {winner} turtle has won! You lose.")
+        turtle.forward(random.randint(0, 20))
 
 
 screen.exitonclick()
